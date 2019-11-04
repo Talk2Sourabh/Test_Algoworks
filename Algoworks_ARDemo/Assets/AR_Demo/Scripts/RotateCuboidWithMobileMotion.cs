85db8eeed9ce1572484e1263f5af67b6ab227641
Talk2Sourabh <sourabh.singhai1990@gmail.com>
2019-11-04 09:47:34 -0800

    ObjectAugmentOnDetectedGroundAndRotatationOfObjectWithMObileMotion

---
+++ Algoworks_ARDemo/Assets/AR_Demo/Scripts/RotateCuboidWithMobileMotion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCuboidWithMobileMotion : MonoBehaviour
+{
+    private void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    private void Update()
+    {
+        Vector3 _previousEulerAngles = transform.eulerAngles;
+        Vector3 _gyroInput = -Input.gyro.rotationRateUnbiased;
+
+        Vector3 _targetEulerAngles = _previousEulerAngles + _gyroInput * Time.deltaTime * Mathf.Rad2Deg;
+        
+        transform.eulerAngles = _targetEulerAngles;
+    }
+}
